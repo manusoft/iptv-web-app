30eb94d3b874e15536ffdf5334fa3c25c32eb22f
manoj@home <manojbabuae@outlook.com>
2024-12-21 01:50:29 +0400

    Add project files.

---
+++ src/LiveTV/Program.cs
@@ -0,0 +1,32 @@
+using LiveTV;
+using LiveTV.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<UrlValidationService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ src/LiveTV/UrlValidationService.cs
@@ -0,0 +1,27 @@
+﻿namespace LiveTV;
+
+public class UrlValidationService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public UrlValidationService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<bool> IsUrlValidAsync(string url)
+    {
+        try
+        {
+            var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Head, url);
+            var response = await client.SendAsync(request);
+
+            return response.IsSuccessStatusCode;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
